e24543955c95fe4d6305bc8289c74ccba6c36821
starseat <starseat3@gmail.com>
2023-03-26 17:00:00 +0900

    임시 페이지 수정

--- src/0301-contact-us.php
+++ src/0301-contact-us.php
@@ -1,1 +1,31 @@
-[견적문의] 준비중 입니다.
+<?php require_once('./fragment/header.php'); ?>
+
+<!-- contents -->
+<div class="content-w-main" id="content-main"><!-- 페이지 속성 분기 "content-w-aaa" -->
+    <div class="contentVisual-w"> <!-- 비주얼 영역 -->
+
+        <?php require_once('./fragment/left.php'); ?>
+
+        <!-- 우측 content-->
+        <div class="contSection-right">
+            <div class="contentVisual-img-w">
+                <img src="./imgs/cont02-0101-top.png" alt="" class="contentVisual-img">
+                <h3 class="sectionTitle">
+                    <strong class="sectionTitle-bg">견적문의</strong>
+                    <span class="sectionTitle-eng">Contact US</span>
+                </h3>
+                
+                <?php require_once('./fragment/right.php'); ?>
+
+            </div>
+            <!-- 콘텐츠 내용-->
+            <div class="contentArticle-w">
+                <h4 class="contentTitle">[견적문의] 준비중 입니다.</h4>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once('./fragment/footer.php'); ?>
+
+<?php require_once('./fragment/tail.php'); ?>
--- src/0302-notice.php
+++ src/0302-notice.php
@@ -1,1 +1,31 @@
-[공지사항] 준비중 입니다.
+<?php require_once('./fragment/header.php'); ?>
+
+<!-- contents -->
+<div class="content-w-main" id="content-main"><!-- 페이지 속성 분기 "content-w-aaa" -->
+    <div class="contentVisual-w"> <!-- 비주얼 영역 -->
+
+        <?php require_once('./fragment/left.php'); ?>
+
+        <!-- 우측 content-->
+        <div class="contSection-right">
+            <div class="contentVisual-img-w">
+                <img src="./imgs/cont02-0101-top.png" alt="" class="contentVisual-img">
+                <h3 class="sectionTitle">
+                    <strong class="sectionTitle-bg">공지사항</strong>
+                    <span class="sectionTitle-eng">Notice</span>
+                </h3>
+                
+                <?php require_once('./fragment/right.php'); ?>
+
+            </div>
+            <!-- 콘텐츠 내용-->
+            <div class="contentArticle-w">
+                <h4 class="contentTitle">[공지사항] 준비중 입니다.</h4>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once('./fragment/footer.php'); ?>
+
+<?php require_once('./fragment/tail.php'); ?>
