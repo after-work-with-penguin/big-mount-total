4b4e3ded6ee4fbd6ffabcf02dbe028259b20a658
hyejin <penguin4338@gamil.com>
2023-04-22 20:56:53 +0900

    모바일 gnb 텍스트 눌러도 서브메뉴 열리게 수정

--- publishing/js/common.js
+++ publishing/js/common.js
@@ -1,8 +1,15 @@
 
 
-//모바일:전체메뉴 열기
+//태블릿:전체메뉴 열기
 function allMenuOpen(){
-    $('.mGnb-w .menu-trigger').on('click',function(){
+    $('.mGnb-open .menu-trigger').on('click',function(){
+       $(this).parents('.mGnb-w').toggleClass('allMenuOpen');      
+       $(this).toggleClass('active');     
+     });
+}
+//모바일:전체메뉴 열기
+function mobileMenuOpen(){
+    $('.mGnb-open').on('click',function(){
        $(this).parents('.mGnb-w').toggleClass('allMenuOpen');      
        $(this).toggleClass('active');     
      });
@@ -16,13 +23,13 @@ function allMenuClose(){
 
 //모바일:전체메뉴-서브열기
 function allMenu1depth(){
-    $('.btnSubOpen01').on('click',function(){
+    $('.btnSubOpen01,.gnbList-link').on('click',function(){
         //$('.gnbList-cont').removeClass('gnb1depthCurrent');
          $(this).parents('.gnbList-cont').toggleClass('gnb1depthCurrent');
     });
 }
 function allMenu2depth(){
-    $('.btnSubOpen').on('click',function(){
+    $('.btnSubOpen,.gnbSub-link').on('click',function(){
         //$('.gnbSub-cont').removeClass('gnb2depthCurrent');
         $(this).parents('.gnbSub-cont').toggleClass('gnb2depthCurrent');
       });
