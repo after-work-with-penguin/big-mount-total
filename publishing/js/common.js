5af67c5fa3910a5b1cdec3f51b699d9caa551fa5
hyejin <penguin4338@gamil.com>
2023-04-26 11:16:18 +0900

    큰 이미지로 교체,  gnb hovere효과 수정

--- publishing/js/common.js
+++ publishing/js/common.js
@@ -17,40 +17,42 @@ function allMenuClose(){
 
 //모바일:전체메뉴-서브열기
 function allMenu1depth(){
-    $('.btnSubOpen01,.gnbList-link').on('click',function(){  // PC + 모바일
-         $(this).parents('.gnbList-cont').toggleClass('gnb1depthCurrent');
-    });
+    // $('.btnSubOpen01,.gnbList-link').on('click',function(){  // PC + 모바일
+    //      $(this).parents('.gnbList-cont').toggleClass('gnb1depthCurrent');
+    // });
 
     // // 모바일
-    // $('.mGnb-w .btnSubOpen01,.gnbList-link').on('click',function(){
-    //   $(this).parents('.gnbList-cont').toggleClass('gnb1depthCurrent');
-    // });
+    $('.mGnb-w .btnSubOpen01,.gnbList-link').on('click',function(){
+      $(this).parents('.gnbList-cont').toggleClass('gnb1depthCurrent');
+    });
 
-    // // pc hover
-    // $('.gnb-w .gnbList-cont').hover(function() {
-    //     $(this).addClass('gnb1depthCurrent');
-    // }, function() {
-    //     $(this).removeClass('gnb1depthCurrent');
-    // });
+    // pc hover
+    $('.gnb-w .gnbList-cont').hover(function() {
+        $('.gnb-w .gnbList-cont').parents('.gnbSub-cont').removeClass('gnb1depthCurrent');
+        $(this).addClass('gnb1depthCurrent');
+    }, function() {
+        $(this).removeClass('gnb1depthCurrent');
+    });
 }
 
 function allMenu2depth(){
-    $('.btnSubOpen,.gnbSub-link').on('click',function(){  // PC + 모바일
+   /* $('.btnSubOpen,.gnbSub-link').on('click',function(){  // PC + 모바일
         $(this).parents('.gnbSub-cont').toggleClass('gnb2depthCurrent');
-      });
+      });*/
 
 
-    // // 모바일
-    // $('.mGnb-w .btnSubOpen,.gnbSub-link').on('click',function(){
-    //   $(this).parents('.gnbSub-cont').toggleClass('gnb2depthCurrent');
-    // });
+    // 모바일
+    $('.mGnb-w .btnSubOpen,.gnbSub-link').on('click',function(){
+      $(this).parents('.gnbSub-cont').toggleClass('gnb2depthCurrent');
+    });
 
-    // // pc hover
-    // $('.gnb-w .gnbSub-w .gnbSub-link-w').hover(function() {
-    //     $(this).parents('.gnbSub-cont').addClass('gnb2depthCurrent');
-    // }, function() {
-    //     $(this).parents('.gnbSub-cont').removeClass('gnb2depthCurrent');
-    // });
+    // pc hover
+    $('.gnb-w .gnbSub-w .gnbSub-link-w').hover(function() {
+        $('.gnb-w .gnbSub-w .gnbSub-link-w').parents('.gnbSub-cont').removeClass('gnb2depthCurrent');
+        $(this).parents('.gnbSub-cont').addClass('gnb2depthCurrent');
+    }, function() {
+        $(this).parents('.gnbSub-cont').removeClass('gnb2depthCurrent');
+    });
 }
 
 // 태블릿 전체메뉴
