a2e1bd4e7aceab89298fd41135b6e05569f63284
starseat <starseat3@gmail.com>
2023-05-02 21:57:07 +0900

    퍼블리싱 및 요청사항 반영

--- src/fragment/footer.php
+++ src/fragment/footer.php
@@ -13,12 +13,12 @@
                     <span class="icon-spot"><img src="./imgs/icon_footerSpot.png" alt=""></span>충청남도 서산시 대산읍 평신1로 531-44
                 </li>
                 <li class="footerList-inner2">사업자등록번호 316-81-21855</li>
-                <li class="footerList-inner2">대표이사 <strong>최 태환</strong></li>
-                <li class="footerList-inner">www.대산토탈.co.kr</li>
+                <li class="footerList-inner2">대표이사 <strong>최태환</strong></li>
+                <li class="footerList-inner"><strong>홈페이지</strong> : www.대산토탈.com</li>
             </ul>
             <ul class="footerList-right">
-                <li class="footerList-inner2"><strong>TEL</strong> : 010-3235-9372</li>
-                <li class="footerList-inner2"><strong>FAX</strong> : 041-663-7332</li>
+                <li class="footerList-inner2"><strong>TEL</strong> : 041-663-7332</li>
+                <li class="footerList-inner2"><strong>FAX</strong> : 041-663-7331</li>
                 <li class="footerList-inner"><strong>E-MAIL</strong> : daesantotal@daesantotal.com</li>
             </ul>
         </div>
