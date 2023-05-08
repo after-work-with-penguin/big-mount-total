9966260eae6b02e0a826c74523cde1f707274c2e
starseat <starseat3@gmail.com>
2023-05-08 12:06:50 +0900

    메타정보 및 네이버 검색 등록

--- src/fragment/head.php
+++ src/fragment/head.php
@@ -10,7 +10,21 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="(주)대산토탈은 20여년간 축적된 노하우로 건설장비, 컨테이너, 도장, 가설재, 특수 자외선 소독기제작 등 임대·판매 설치를 저렴한 가격 과 최고의 품질, 다양한 제품으로 고객만족과 행복을 위하여 최선의 서비스를 다하고 있습니다.">
+    <meta name="keywords" content="건설장비, 컨테이너, 도장, 가설재, 특수 자외선 소독기제작">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="대산토탈">
+    <meta property="og:description" content="(주)대산토탈은 20여년간 축적된 노하우로 건설장비, 컨테이너, 도장, 가설재, 특수 자외선 소독기제작 등 임대·판매 설치를 저렴한 가격 과 최고의 품질, 다양한 제품으로 고객만족과 행복을 위하여 최선의 서비스를 다하고 있습니다.">
+    <meta property="og:image" content="https://www.xn--vk1bp3xfrmxya.com/imgs/logo.png">
+    <meta property="og:url" content="http://www.대산토탈.com">
+
+    <meta name="naver-site-verification" content="0629c61973a77e4459f667f3e06d98e3a42d2fb9" />
     <title>대산토탈</title>
+    <link rel="canonical" href="http://www.대산토탈.com">
 
     <link rel="stylesheet" type="text/css" href="./vendor/slick/slick.css" media="all" />
     <link rel="stylesheet" type="text/css" href="./css/common.css" media="all" />
